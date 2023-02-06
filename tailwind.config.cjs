46b5f52768d47c2c4547e2ad9f07a9803931aece
Boston343 <branden.austin1@gmail.com>
2023-02-06 14:26:42 -0500

    small adjustments to hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
       sm: "480px",
       md: "768px",
       lg: "976px",
-      xl: "1440px",
+      // xl: "1440px",
+      xl: "1200px",
     },
     extend: {
       colors: {
