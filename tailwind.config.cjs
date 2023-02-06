c1182668411e6c4d8f70d6d38b3dff7d59d856d8
Boston343 <branden.austin1@gmail.com>
2023-02-06 16:26:11 -0500

    Features section done, with additional small adjustments

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     screens: {
-      sm: "480px",
+      // sm: "480px",
+      sm: "560px",
       md: "768px",
       lg: "976px",
       // xl: "1440px",
